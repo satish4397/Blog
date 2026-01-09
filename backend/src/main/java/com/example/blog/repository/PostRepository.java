0e6987767019ec7b39b6c203d8c8e18e1041976e
Satish Chaurasiya <satishchaurasia59@gmail.com>
2026-01-09 19:05:15 +0530

    removed

--- backend/src/main/java/com/example/blog/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog.repository;
-
-import com.example.blog.entity.PostEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<PostEntity, String> {
-}
